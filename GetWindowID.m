4c47c228f7b3147d21fa99be000ef48aeb9f628a
Matt Clark <matt@codified.co>
2015-04-16 15:37:04 -0400

    match windows on prefix

--- GetWindowID.m
+++ GetWindowID.m
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
 	NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements,kCGNullWindowID);
 	for(NSDictionary *window in windows)
-		if ([[window objectForKey:(NSString *)kCGWindowOwnerName] isEqualToString:[NSString stringWithUTF8String:argv[1]]])
-			if ([[window objectForKey:(NSString *)kCGWindowName] isEqualToString:[NSString stringWithUTF8String:argv[2]]])
+		if ([[window objectForKey:(NSString *)kCGWindowOwnerName] hasPrefix:[NSString stringWithUTF8String:argv[1]]])
+			if ([[window objectForKey:(NSString *)kCGWindowName] hasPrefix:[NSString stringWithUTF8String:argv[2]]])
 				printf("%d\n", [[window objectForKey:(NSString *)kCGWindowNumber] intValue]);
 }
